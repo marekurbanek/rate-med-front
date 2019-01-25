f0bc5d1b73ac60430188432f18c9b77d493eabef
Urbanek <marek.urbanek@accenture.com>
2019-01-25 17:28:40 +0100

    Multiple specialities working

--- src/app/doctors/doctors-list/doctors-list.component.ts
+++ src/app/doctors/doctors-list/doctors-list.component.ts
@@ -17,11 +17,36 @@ export class DoctorsListComponent implements OnInit {
 
   getDoctorsList(): void {
     this.doctorsService.getDoctors().subscribe(
-      doctors => {
-        this.doctors = doctors;
-      },
-      error => console.log(error)
+      response => {
+        this.doctors = this.getDoctors(response);
+      }
     );
+  }
+
+  getDoctors(doctorsResponse): IDoctor[] {
+    const removeDuplicates = (myArr, prop) => {
+      return myArr.filter((obj, pos, arr) => arr.map(mapObj => mapObj[prop]).indexOf(obj[prop]) === pos);
+    }
+    
+    let distinctDoctors = removeDuplicates(doctorsResponse, 'id')
+
+    let specialities = doctorsResponse.map(doctor => {
+      return {
+        doctorId: doctor.id,
+        speciality: doctor.speciality
+      }
+    })
+
+    specialities.forEach(spec => {
+      let doctor = distinctDoctors.find( doctor => doctor.id === spec.doctorId)
+      if(typeof doctor.speciality === 'string') {
+        doctor.speciality = [`${spec.speciality}`]
+      } else {
+        doctor.speciality.push(spec.speciality)
+      }
+    })
+
+    return distinctDoctors
   }
 
   ngOnInit(): void {
