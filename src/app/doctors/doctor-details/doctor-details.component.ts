9c725c331c45f3f1d769ac9aa0b997e492b44cb0
Urbanek <marek.urbanek@accenture.com>
2019-01-28 11:06:17 +0100

    Refactored getting list and detailed doctor

--- src/app/doctors/doctor-details/doctor-details.component.ts
+++ src/app/doctors/doctor-details/doctor-details.component.ts
@@ -30,35 +30,8 @@ export class DoctorDetailsComponent implements OnInit {
   ngOnInit() {
     const id = this.route.snapshot.params['id'];
     this.doctorsService.getDoctor(id).subscribe((doctor) => {
-      this.doctor = this.getDoctors(doctor);
-      console.log(this.doctor)
+      this.doctor = doctor
     });
-  }
-
-  getDoctors(doctorsResponse) {
-    const removeDuplicates = (myArr, prop) => {
-      return myArr.filter((obj, pos, arr) => arr.map(mapObj => mapObj[prop]).indexOf(obj[prop]) === pos);
-    }
-
-    let distinctDoctors = removeDuplicates(doctorsResponse, 'id')
-
-    let specialities = doctorsResponse.map(doctor => {
-      return {
-        doctorId: doctor.id,
-        speciality: doctor.speciality
-      }
-    })
-
-    specialities.forEach(spec => {
-      let doctor = distinctDoctors.find( doctor => doctor.id === spec.doctorId)
-      if(typeof doctor.speciality === 'string') {
-        doctor.speciality = [`${spec.speciality}`]
-      } else {
-        doctor.speciality.push(spec.speciality)
-      }
-    })
-
-    return distinctDoctors[0]
   }
 
 }
--- src/app/doctors/doctors-list/doctors-list.component.ts
+++ src/app/doctors/doctors-list/doctors-list.component.ts
@@ -18,35 +18,9 @@ export class DoctorsListComponent implements OnInit {
   getDoctorsList(): void {
     this.doctorsService.getDoctors().subscribe(
       response => {
-        this.doctors = this.getDoctors(response);
+        this.doctors = response;
       }
     );
-  }
-
-  getDoctors(doctorsResponse): IDoctor[] {
-    const removeDuplicates = (myArr, prop) => {
-      return myArr.filter((obj, pos, arr) => arr.map(mapObj => mapObj[prop]).indexOf(obj[prop]) === pos);
-    }
-    
-    let distinctDoctors = removeDuplicates(doctorsResponse, 'id')
-
-    let specialities = doctorsResponse.map(doctor => {
-      return {
-        doctorId: doctor.id,
-        speciality: doctor.speciality
-      }
-    })
-
-    specialities.forEach(spec => {
-      let doctor = distinctDoctors.find( doctor => doctor.id === spec.doctorId)
-      if(typeof doctor.speciality === 'string') {
-        doctor.speciality = [`${spec.speciality}`]
-      } else {
-        doctor.speciality.push(spec.speciality)
-      }
-    })
-
-    return distinctDoctors
   }
 
   ngOnInit(): void {
